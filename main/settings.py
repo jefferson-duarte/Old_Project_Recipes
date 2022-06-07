156a10322c397e1996af2a25daa0f69dd500eb81
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-07 23:05:43 +0100

    Criado urls e views

--- main/settings.py
+++ main/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates',],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', context={'name': 'Jefferson Duarte'})
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
