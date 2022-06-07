baadb164b41ad5de9490d197fd550c62d8c396c9
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-07 23:30:53 +0100

    Configurado templates recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contato/', views.contato, name='contato'),
-    path('sobre/', views.sobre, name='sobre'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Jefferson Duarte'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Jefferson Duarte'})
 
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
