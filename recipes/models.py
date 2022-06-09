64a967a6dd0d6d5aa2f7c10ad41ff53b392f17c1
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-09 10:35:32 +0100

    Adicionado os campos administrativos

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -23,3 +26,6 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
