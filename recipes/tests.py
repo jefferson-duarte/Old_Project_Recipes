04cac6be192113a65d1ca89a2b1b576c1e6104dd
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-11 11:33:46 +0100

    Configurado testes nas views

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,16 @@
+from django.urls import reverse
 from django.test import TestCase
 
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1, 'Um é igual a um'
+    def test_recipe_home_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def teste_recipe_details_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
