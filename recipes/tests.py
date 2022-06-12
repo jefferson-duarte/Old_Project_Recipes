4aab52e54d16b8595503b9a63339e3c17ce46748
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-12 09:23:42 +0100

    Realizado testes das funções

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
-from django.urls import reverse
+from django.urls import reverse, resolve
 from django.test import TestCase
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
@@ -14,3 +15,21 @@ class RecipeURLsTest(TestCase):
     def teste_recipe_details_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertEqual(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
