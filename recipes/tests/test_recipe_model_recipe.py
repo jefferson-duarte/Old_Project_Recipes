2c38f67244d2414f423171a831f7deddd00be6de
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-14 11:18:24 +0100

    Configurado teste __str__ de category e recipe

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -50,3 +50,12 @@ class RecipeModelTest(RecipeTestBase):
         self.assertFalse(
             recipe.is_published, msg='Recipe is_published is not False.',
         )
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.title = 'Testing Representation'
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe), needed, msg=f'Recipe string representation must be "{needed}" but "{str(self.recipe)}" was received.'
+        )
