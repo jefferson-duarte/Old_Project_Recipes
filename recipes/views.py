1dd4bfd97e009d918f2c0defd088a05bb29ff649
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-19 23:07:21 +0100

    Configurado a função busca por recipes

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
+from django.db.models import Q
 
 
 def home(request):
@@ -43,6 +44,13 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
