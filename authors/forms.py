ff3e0826e715cad14dbad7868516125366f343f8
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-27 12:24:44 +0100

    Adicionado validação no campo password

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
@@ -75,3 +76,38 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atencao' in data:
+            raise ValidationError(
+                'Nao digite %(value)s no campo password.',
+                code='Invalid',
+                params={'value': '"atencao"'}
+            )
+
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+
+        if 'Jefferson Duarte' in data:
+            raise ValidationError(
+                'Nao digite %(value)s no campo first name.',
+                code='Invalid',
+                params={'value': '"Jefferson Duarte"'}
+            )
+
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password': 'Password and password2 must be equal.'
+            })
