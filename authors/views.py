07f34974f44219f74500d6f5f98f4bd2fa3fdac2
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-30 23:54:50 +0100

    Organizado arquivos

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .forms import RegisterForm
 from django.http import Http404
 from django.contrib import messages
+from django.urls import reverse
 
 
 def register_view(request):
@@ -9,6 +10,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:create'),
     })
 
 
