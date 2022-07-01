c1fe22ac5182f0bbb1b07d9eb3adffd15516bfdf
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-01 11:54:55 +0100

    Add logout no formulário

--- authors/views.py
+++ authors/views.py
@@ -3,7 +3,8 @@ from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def register_view(request):
@@ -30,6 +31,7 @@ def register_create(request):
         messages.success(request, 'Your user is created, please log in.')
 
         del(request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
 
@@ -45,16 +47,16 @@ def login_view(request):
 def login_create(request):
     if not request.POST:
         raise Http404
-    
+
     form = LoginForm(request.POST)
     login_url = reverse('authors:login')
-    
+
     if form.is_valid():
         authenticated_user = authenticate(
             username=form.cleaned_data.get('username', ''),
             password=form.cleaned_data.get('password', ''),
         )
-        
+
         if authenticated_user is not None:
             messages.success(request, 'You are logged in.')
             login(request, authenticated_user)
@@ -63,3 +65,18 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password.')
     return redirect(login_url)
+
+
+@login_required(
+    login_url='authors:login',
+    redirect_field_name='next',
+)
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+
+    logout(request)
+    return redirect(reverse('authors:login'))
