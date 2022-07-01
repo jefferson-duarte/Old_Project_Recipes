acb77030a628e6556a64a74b8f94ad168ed59cab
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-01 10:33:26 +0100

    Criado o formulário de login

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
@@ -34,7 +34,11 @@ def register_create(request):
 
 
 def login_view(request):
-    return render(request, 'authors/pages/login.html')
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
 
 
 def login_create(request):
