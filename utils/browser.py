117617eaca315720c969f3b4c51eec253ede4a7b
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-02 09:41:58 +0100

    Teste funcional home page

--- utils/browser.py
+++ utils/browser.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from pathlib import Path
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+import os
 
 ROOT_PATH = Path(__file__).parent.parent
 CHROMEDRIVER_NAME = 'chromedriver.exe'
@@ -11,11 +12,14 @@ CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
 
 def make_chrome_browser(*options):
     chrome_options = webdriver.ChromeOptions()
-    
+
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
-            
+
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
+
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
@@ -24,6 +28,6 @@ def make_chrome_browser(*options):
 
 if __name__ == '__main__':
     browser = make_chrome_browser()
-    browser.get('http://www.globo.com')
+    browser.get('http://www.google.com')
     sleep(2)
     browser.quit()
