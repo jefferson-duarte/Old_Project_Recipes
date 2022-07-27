41edc00be173a2a713dab061e749df200133fa6c
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-27 09:02:40 +0100

    Adicionado teste pagination e home page

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from .base import RecipeBaseFunctionalTest
 import pytest
 from unittest.mock import patch
@@ -11,3 +12,48 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here.', body.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_sarch_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what I need'
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê um campo de busca com o texto "Saerch for a recipe"
+        search_input = self.browser.find_element(
+            By.XPATH, '//input[@placeholder="Search for a recipe."]'
+        )
+
+        # Clica neste input e digita o termo de busca
+        # "Recipe Title 1" para encontrar a receita com esse título
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
+
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê que tem uma paginação e clica na página 2
+        page2 = self.browser.find_element(
+            By.XPATH, '//a[@aria-label="Go to page 2"]'
+        )
+        page2.click()
+
+        # Vê que tem mais 2 receitas na página 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')), 2
+        )
+        self.sleep(5)
